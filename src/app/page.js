f7c59b498e264eeb7eeb5048dc205e2f2dd6836f
ferflm <ferflmtzzz@gmail.com>
2025-09-13 00:21:57 -0600

    Redirected to homepage, Avatar componets and Dropdown-menu component added

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
+"use client"
+
+import UserButton from "@/components/user_button";
+import { SessionProvider } from "next-auth/react";
+
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      Home Page
+    <div>
+      <SessionProvider>
+        <UserButton/>
+      </SessionProvider>
     </div>
   );
 }
